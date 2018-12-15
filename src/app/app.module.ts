f5a70aec200243dbd1eeb1185f3529f51a19df1d
David Mortkowitz <dmortkowitz@gmail.com>
2018-12-14 16:15:38 -0800

    Original folder went corrupt, somehow. After reinstalling everything I know how to reinstall, migrating to new folder.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { routing } from './app.routing';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
-import { masterFirebaseConfig } from './api-keys';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { MainpageComponent } from './mainpage/mainpage.component';
-import { UsershelfComponent } from './usershelf/usershelf.component';
-import { BookshelfComponent } from './bookshelf/bookshelf.component';
-import { SubmitbookComponent } from './submitbook/submitbook.component';
-
-
-export const firebaseConfig = {
-  apiKey: masterFirebaseConfig.apiKey,
-  authDomain: masterFirebaseConfig.authDomain,
-  databaseURL: masterFirebaseConfig.databaseURL,
-  storageBucket: masterFirebaseConfig.storageBucket
-};
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainpageComponent,
-    UsershelfComponent,
-    BookshelfComponent,
-    SubmitbookComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    routing,
-    AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireDatabaseModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
